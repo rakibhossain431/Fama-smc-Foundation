30e6e8a42b7e27a93211f5539109c2738052da19
Rakib Hossain <rakibjp431@gmail.com>
2023-03-20 13:19:01 +0600

    Add Pag Developerse

---
+++ src/Components/DevelopersResources/DevelopersResources.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+function DevelopersResources() {
+    return (
+        <section class="py-5">
+            <div class="container">
+             <div class="col-md-11 col-12 m-auto developer-resources">
+                <div class=" col-12 col-lg-8 pb-5">
+                    <h2 class="brand-card-title">Developer Resources </h2>
+                    <p>Take a look at some of our developer resources. We have a separate website to host all the resources and documentation for developers like you.</p>
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 cols-lg-3 g-5 home-card-gap">
+                    <div class="col">
+                    <div class="card-group">
+                        <div class="card all-brands-card">
+                            <div className=''>
+                             <img src="./images/Labs/image (4).png" alt="img" />
+                            </div>
+                            <div class="card-body all-brands-card-body">
+                                <h6 class="impact-card-title">Curabitur tempor quis eros tempus</h6>
+                                <p class="card-text all-brands-card-text">Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Curabitur tempor quis eros tempus lacinia.</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col">
+                        <div class="card-group">
+                            <div class="card all-brands-card">
+                                <div className=''>
+                                     <img src="./images/Labs/image (6).png" alt="img" />
+                                </div>
+                            <div class="card-body all-brands-card-body">
+                                <h6 class="impact-card-title">Curabitur tempor quis eros tempus</h6>
+                                <p class="card-text all-brands-card-text">Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Curabitur tempor quis eros tempus lacinia.</p>
+                            </div>
+                            </div>
+                    </div>
+                    </div>
+                    <div class="col">
+                        <div class="card-group">
+                            <div class="card all-brands-card">
+                                <div className=''>
+                                   <img src="./images/Labs/image (5).png" alt="img" />
+                                </div>
+                                <div class="card-body all-brands-card-body">
+                                    <h6 class="impact-card-title">Curabitur tempor quis eros tempus</h6>
+                                    <p class="card-text all-brands-card-text">Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Curabitur tempor quis eros tempus lacinia.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+       </section>
+    )
+}
+
+export default DevelopersResources
